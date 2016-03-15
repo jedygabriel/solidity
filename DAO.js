527c349562b3bc6e4e7486ccfaf8de3d67c17de6
Jedygabriel <jedygabriel@gmail.com>
2016-03-15 11:00:09 -0300

    DAO criação script

--- DAO.js
+++ DAO.js
@@ -1,1 +1,23 @@
-DAO.js
+contract owned {
+	address public owner;
+
+	function owned() {
+		owner = msg.sender;
+	}
+
+	modifier onlyOwner {
+		if (msg.sender != owner) throw;
+		_
+	}
+
+	function transferOwnership(address newOwner) onlyOwner {
+		owner = newOwner;
+	}
+}
+
+contract Congress is owned {
+	//Variaveis do contrato e dos eventos
+	uint public minimumQuorum;
+	uint public debatingPeriodInMinutes;
+
+}
